b298205cbe46b137b3726e266f9fe7681676a4fb
karansenpai <karandeoli444@gmail.com>
2024-05-31 18:31:06 +0530

    Authentication using NextAuth

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,39 @@
-import '@/styles/globals.css'
-
+import Navbar from "@/components/Navbar";
+import { Toaster } from "@/components/ui/Toaster";
+import { cn } from "@/lib/utils";
+import "@/styles/globals.css";
+import { Inter } from "next/font/google";
 export const metadata = {
-  title: 'CodeAura',
-  description: 'A Reddit clone built with Next.js and TypeScript.',
-}
+  title: "CodeAura",
+  description: "A social media platform for Devs",
+};
+
+const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang='en'>
-      <body>{children}</body>
+    <html
+      lang="en"
+      className={cn(
+        "bg-white text-slate-900 antialiased light",
+        inter.className
+      )}
+    >
+      <body className="min-h-screen pt-12 bg-slate-50 antialiased">
+
+        <Navbar/>
+        <div className="container max-w-7xl mx-auto h-full pt-12">
+
+        {children}
+        </div>
+
+        <Toaster/>
+        
+      </body>
     </html>
-  )
+  );
 }
